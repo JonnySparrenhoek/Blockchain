bc2e72c53e0d95950e3899211f734f0ee3e038f2
Jonny Sparrenhök <Jonnysp@kth.se>
2019-01-13 04:29:04 +0800

    blockchain project

---
+++ contracts/FiatContract.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.24;
+// As of truffle 5.0.1 "constant" is not longer available
+/*contract FiatContract {
+    function ETH(uint _id) public constant returns (uint256);
+    function USD(uint _id)  public constant returns (uint256);
+    function EUR(uint _id) public constant returns (uint256);
+    function GBP(uint _id) public constant returns (uint256);
+    function updatedAt(uint _id) public constant returns (uint);
+}*/
